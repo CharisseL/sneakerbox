848b7aa11259bce8bd62ba0f769a57b16b1d174f
CharisseL <charisse8@gmail.com>
2014-12-15 15:54:52 -0600

    controllers done. added add'l angular files.

---
+++ js/constants.js
@@ -0,0 +1,6 @@
+;(function(){
+	'use strict';
+	angular.module('snkrApp')
+		.constant('FIREBASE_URL', 'https://sneakerbox/firebaseio.com')
+
+	}());
---
+++ js/controllers.js
@@ -0,0 +1,59 @@
+;(function(){
+	'use strict';
+
+	angular.module('snkrApp')
+		.controller('ShowController', function($routeParams, sneakerFactory){
+			var vm = this;
+			var id = $routeParams.id;
+			sneakerFactory.getShoe(id, function(data){
+				vm.shoe = data;
+			});
+		})
+		.controller('EditController', function($routeParams, sneakerFactory){
+			var vm = this;
+			var id = $routeParams.id;
+
+			sneakerFactory.getShoe(id, function(data){
+				vm.newShoe = data;
+			});
+
+			vm.addNewShoe = function(){
+				sneakerFactory.editShoe(id, vm.newShoe);
+			};
+
+			vm.sportOptions = sneakerFactory.sportOptions;
+
+		})
+		.controller('ShoeController', function($http, sneakerFactory{
+			var vm = this;
+
+			sneakerFactory.getAllShoes(function(data){
+				vm.shoes = data;
+			});
+
+			vm.addNewShoe = function(){
+				sneakerFactory.createShoe(vm.newShoe, function(data){
+					vm.shoes[data.name] = vm.newShoe;
+					vm.newShow = _freshShoe();
+				});
+			};
+
+			vm.removeShoe = function(shoeId){
+				sneakerFactory.deleteShoe(shoeId, function(){
+					delete vm.shoes[shoeId];
+				});
+			};
+
+			vm.newShoe = _freshShoe();
+
+			vm.sportOptions = sneakerFactory.sportOptions;
+
+			function _freshShoe(){
+				return{
+					sport: 'basketball'
+				};
+			}
+
+		});
+
+	}());
