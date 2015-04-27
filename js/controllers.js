75827d98017dcf2529bfd04165188b6534ff597e
CharisseL <charisse8@gmail.com>
2015-04-27 01:45:02 -0500

    Top 5 drop alert

---
+++ js/controllers.js
@@ -0,0 +1,59 @@
+;(function(){
+	'use strict';
+
+	angular.module('snkrApp')
+		.controller('ShowController', function($routeParams, sneakerFactory){
+			var vm = this;
+			var id = $routeParams.id;
+			sneakerFactory.getShoe(id, function(data){
+				vm.shoe = data;
+			});
+		})
+		.controller('EditController', function($routeParams, sneakerFactory){
+			var vm = this;
+			var id = $routeParams.id;
+
+			sneakerFactory.getShoe(id, function(data){
+				vm.newShoe = data;
+			});
+
+			vm.addNewShoe = function(){
+				sneakerFactory.createShoe(id, vm.newShoe);
+			};
+
+
+		})
+		.controller('ShoeController', function($http, sneakerFactory){
+			var vm = this;
+
+			sneakerFactory.getAllShoes(function(data){
+				vm.shoes = data;
+			});
+
+			vm.addNewShoe = function(){
+				sneakerFactory.createShoe(vm.newShoe, function(data){
+					vm.shoes = vm.shoes || {};
+					vm.shoes[data.name] = vm.newShoe;
+					vm.newShoe = _freshShoe();
+				});
+			};
+
+			vm.removeShoe = function(shoeId){
+				sneakerFactory.deleteShoe(shoeId, function(){
+					delete vm.shoes[shoeId];
+				});
+			};
+
+			vm.newShoe = _freshShoe();
+
+			vm.sportOptions = sneakerFactory.sportOptions;
+
+			function _freshShoe(){
+				return{
+					sport: 'basketball'
+				};
+			}
+
+		});
+
+	}());
